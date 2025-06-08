3db8f7b7bd401b158d766f7149935c7df6e76ffd
Chriq <christiancook52@gmail.com>
2025-06-08 19:55:27 -0400

    final oopsies

--- Components/SplashScreen/SplashScreen.cs
+++ Components/SplashScreen/SplashScreen.cs
@@ -1,15 +1,13 @@
 using Godot;
 using System;
 
-public partial class SplashScreen : Node
-{
+public partial class SplashScreen : Node {
 	[Export] private CanvasItem splashScreen;
 	[Export] private FadeController fade;
 	[Export] private PackedScene scene;
 
 	public override async void _Ready() {
 		splashScreen.Modulate = new Color(splashScreen.Modulate, 0f);
-		Input.MouseMode = Input.MouseModeEnum.Hidden;
 
 		await ToSignal(GetTree().CreateTimer(1f), "timeout");
 		await fade.FadeIn(splashScreen);
